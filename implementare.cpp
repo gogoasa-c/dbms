722de858fa773ac722f88334840b1c43dd684223
Brescar <ghitavalentin2001@gmail.com>
2022-11-19 23:30:26 +0200

    UPDATE!! PARE SA MEARGA!!! ps: n-am stat sa testez prea mult dar seems alright?>

--- implementare.cpp
+++ implementare.cpp
@@ -2,7 +2,6 @@
 #include "clase_si_functii.h"
 #include <iostream>
 #include <string>
-#define REAL_SIZE 100
 using namespace std;
 
 string take_user_input_and_convert_lowercase()
@@ -20,14 +19,14 @@ string take_user_input_and_convert_lowercase()
 
 string* split_string_into_words(string userInput)
 {
-	string word[REAL_SIZE] = {""};									//aici este probabil sa trebuiasca sa alocam dinamic daca permitem introducerea a prea multe coloane in tabele. nota: pt introducerea din fisiere, comenzile for vi despartite intre ele prin enter
-	int i;
-	/*for (i = 0; i < REAL_SIZE; i++) {								//daca alocam dinamic	
+	int controlSize = 100;											//controlam size-ul array-ului de string alocat dinamic
+	string* word = new string[controlSize];							//nota: pt introducerea din fisiere, comenzile for vi despartite intre ele prin enter
+	string* aux = NULL;
+	int i, j = 0, k;												//i = initial contor apoi index pt a cata litera din string ;; j = index pt al catelea cuvant formam ;; k = index similar lui j dar folosim pt resize de word prin ajutorul lui aux 
+	for (i = 0; i < controlSize; i++) {
 		word[i] = "";
-	}*/
-	int j = 0;														//index pt al catelea cuvant formam 
+	}
 	word[0] = j;													//!!!ATENTIE!!! cuvant[0] va tine NR DE CUVINTE (pt a folosi in alocari dinamice vom folosi **stoi(cuvant[0])**
-	//int controlSize = 0;											//controlam size-ul array-ului de string alocat dinamic
 	unsigned int startedTyping = 0;									//ne ajuta sa ignoram multiple space-uri, pt a nu da eroare usor
 	for (i = 0; i < userInput.size(); i++) {
 		if (userInput[i] != ' ') {
@@ -35,20 +34,28 @@ string* split_string_into_words(string userInput)
 		}
 		if (startedTyping) {										//adica != 0
 			j++;													//odata ce nu avem space-uri, se ajunge la primul cuvant, apoi al doilea si tot asa
-			word[0] = j;
-			/*if (j >= controlSize) {								//pentru alocare dinamica
+			word[0] = j;											//word[0] = "5". stoi(word[5] == int 5); IMPORTANT! vom folosi valoarea stocata la word[0] pt a sti cate argumente avem. daca sunt 5, argumentele se vor afla pe pozitiile interval [1, 5]
+			if (j >= controlSize) {									//pentru alocare dinamica
+				string* aux = new string[controlSize];
+				for (k = 0; k < controlSize; k++) {
+					aux[k] = word[k];
+				}
 				controlSize += 100;
-
 				delete[] word;
 				word = new string[controlSize];
-			}*/
+				for (k = 0; k < controlSize - 100; k++) {
+					word[k] = aux[k];
+				}
+			}
 			while (userInput[i] != ' ' && i < userInput.size()) {
-				word[j] += userInput[i++];						//cat timp nu avem space-uri, adugam literele la cuvantul j pt a forma intregul argument cuvant[j]
+				word[j] += userInput[i++];							//cat timp nu avem space-uri, adugam literele la cuvantul j pt a forma intregul argument cuvant[j]
 			}
 			startedTyping = 0;										//s-a ajuns la space, astfel ca incetam tastarea/adugarea argumentului trecut si trecem mai departe
 		}
 	}
 
+	//delete[] word													//asta se va face probabil in constructorii din clasele care isi creaza metodele pe baza argumentelor functiilor sql
+	delete[] aux;
 	return word;
 }
 
--- sqlite.cpp
+++ sqlite.cpp
@@ -9,17 +9,9 @@ using namespace std;
 int main()
 {
     cout << "Alpha v1.1\n";
-    cout << "ATENTIE: ENTER confirma inputul/comanda introdusa. EXIT incheie executia. Daca doriti anularea unei comenzi introduceti o comanda eronata.\n\n"; //neimplementat inca
+    cout << "ATENTIE: ENTER confirma inputul/comanda introdusa. EXIT incheie executia. Daca doriti anularea unei comenzi introduceti o comanda eronata.\n\n";   //neimplementat inca
 
-    //exemplificat pe pasi ar trebui sa functioneze asa insa string v[100] nu este valoare asignabila si da eroare la functia split_...
-    /*string v[100];
-    string test = take_user_input_and_convert_lowercase();
-    string v[100] = split_string_into_words(test);
-    int rez = identify_command_type(v);*/
-
-    //std::copy(split_string_into_words(test), split_string_into_words(test) + 99, v); // asa da eroare dar pe net zice ca e o metoda sa copiezi oarecum?
-
-    //cout << identify_command_type(split_string_into_words(take_user_input_and_convert_lowercase())); //ideal ar trebui sa mearga asa dar da eroare
+    cout << identify_command_type(split_string_into_words(take_user_input_and_convert_lowercase()));                                                            //FUNCTIONEAZA
 
     return 0;
 }
