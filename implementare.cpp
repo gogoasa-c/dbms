d633a1942b6cf8343e5951682ea078f1ec2bc0fd
Brescar <ghitavalentin2001@gmail.com>
2022-12-20 16:53:42 +0200

    na, va pup, nu mai am ce sa comentez

--- implementare.cpp
+++ implementare.cpp
@@ -1561,80 +1561,34 @@ void readFromCsvFiles(fstream& f, vector<Table>& t) {
 
 void import_data_from_csv_file_to_existing_table(ifstream& f, vector<Table>& t, int poz) {
 
-	string aux;
+	string aux, aux1;
 	int nrArguments = t[poz].getHeader().getNrColumns();
 	string final;
 	
-	while (getline(f, aux))
+	while (getline(f, aux1))
 	{
-
+		final = "";
 		int nrArgTotal = 0;
-		string* command = split_string_into_words_comma(aux);
+		string* command = split_string_into_words(aux1);
 		string aux2;
 
-		nrArgTotal += stoi(command[0]) + 4;
-		final += to_string(nrArgTotal);
-		final += ",insert,into,table,values";
+		final += "insert into ";
+		final += (t[poz].getName());
+		final += " values ";
 		for (int i = 1; i <= stoi(command[0]); i++)
 		{
-			aux2 += ",";
+			aux2 += " ";
 			aux2 += command[i];
 		}
 		final += aux2;
+		//final += ')';
 
 		delete[] command;
 
-		/*stringstream ss(final);
-		command = new string[stoi(final)];*/
-
-		command = split_string_into_words_comma(final);
-		t[poz].addEntry(nrArguments, command, t[poz]);
-		delete[] command;
+		command = split_string_into_words(final);
+		identify_command_type(command, t);
+		//delete[] command; cumva da eorare now sure why doesn;t make sense to me
 	}
-}
-
-string* split_string_into_words_comma(string userInput)
-{
-	if (userInput == "invalid") {
-		return nullptr;
-	}
-	int controlSize = 100;											//controlam size-ul array-ului de string alocat dinamic
-	string* word = new string[controlSize];							//nota: pt introducerea din fisiere, comenzile for vi despartite intre ele prin enter ;; word[index] = ""
-	string* aux = NULL;
-	int i, numberWords = 0, k;										//i = initial contor apoi index pt a cata litera din string ;; numberWords = index pt al catelea cuvant formam ;; k = index similar lui numberWords dar folosim pt resize de word prin ajutorul lui aux 
-	word[0] = to_string(numberWords);								//!!!ATENTIE!!! cuvant[0] va tine NR DE CUVINTE (pt a folosi in alocari dinamice vom folosi **stoi(cuvant[0])**
-	unsigned int startedTyping = 0;									//ne ajuta sa ignoram multiple space-uri, pt a nu da eroare usor
-	for (i = 0; i < userInput.size(); i++) {
-		if (userInput[i] != ',') {
-			startedTyping++;
-		}
-		if (startedTyping) {										//adica != 0
-			numberWords++;											//odata ce nu avem space-uri, se ajunge la primul cuvant, apoi al doilea si tot asa
-			word[0] = to_string(numberWords);						//word[0] = "5". stoi(word[5] == int 5); IMPORTANT! vom folosi valoarea stocata la word[0] pt a sti cate argumente avem. daca sunt 5, argumentele se vor afla pe pozitiile interval [1, 5]
-			if (numberWords >= controlSize) {						//pentru alocare dinamica
-				string* aux = new string[controlSize];
-				for (k = 0; k < controlSize; k++) {
-					aux[k] = word[k];
-				}
-				controlSize += 100;
-				delete[] word;
-				word = new string[controlSize];
-				for (k = 0; k < controlSize - 100; k++) {
-					word[k] = aux[k];
-				}
-			}
-			while (userInput[i] != ',' && i < userInput.size()) {
-				word[numberWords] += userInput[i++];				//cat timp nu avem space-uri, adugam literele la cuvantul numberWords pt a forma intregul argument cuvant[numberWords]
-			}
-			startedTyping = 0;										//s-a ajuns la space, astfel ca incetam tastarea/adugarea argumentului trecut si trecem mai departe
-		}
-	}
-
-	//!!!NU UITAM SA DEZALOCAM MEMORIA DUPA CE NU MAI AVEM NEVOIE DE VECTORUL DE CUVINTE!!!
-
-	//delete[] word													//asta se va face probabil in constructorii din clasele care isi creaza metodele pe baza argumentelor functiilor sql
-	delete[] aux;
-	return word;
 }
 
 string* split_string_into_words_comma(string userInput)
